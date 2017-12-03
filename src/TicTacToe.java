29839e791b430fa4ea326a759374572df29267cb
Rishav Rout <rout.rishav@gmail.com>
2017-12-03 03:20:11 -0600

    typo

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,4 @@
 
 public class TicTacToe{
-	// @Aditya add whatever you want here or change anything ot make running smooth
+	// @Aditya add whatever you want here or change anything to make running smooth
 }
